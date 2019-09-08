d0eed393a2ac18e546d10449577cac168f1b5c0a
harrylincoln <harrylincoln0612@gmail.com>
2019-09-08 16:18:21 +0100

    deploy script for s3

---
+++ scripts/deploy.js
@@ -0,0 +1,40 @@
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+const fs = require('fs');
+const glob = require('glob');
+const mimeTypes = require('mime-types');
+require('dotenv').config();
+
+AWS.config = new AWS.Config({
+    credentials: new AWS.Credentials({
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    }),
+    region: process.env.AWS_DEFAULT_REGION
+});
+
+glob('./build/**/*', {}, (err, files) => {
+    files.forEach(file => {
+        if (!fs.lstatSync(file).isDirectory()) {
+            const fileContents = fs.readFileSync(`./${file}`);
+            const fileMime = mimeTypes.lookup(file);
+
+            s3.upload(
+                {
+                    Bucket: 'notate-app',
+                    Key: file.replace('./public/', ''),
+                    Body: fileContents,
+                    ContentType: fileMime
+                },
+                {partSize: 10 * 1024 * 1024, queueSize: 1},
+                (err, data) => {
+                    if (err) {
+                        throw new Error(err.message);
+                    }
+
+                    console.log(data);
+                }
+            );
+        }
+    });
+});
