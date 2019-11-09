6aee9f4da41960f4f1bf8a0cd0b2dcf8e1b91b0d
Harry Lincoln <harrylincoln0612@gmail.com>
2019-11-09 16:55:24 +0000

    App.js test coverage (#14)
    * App.js test coverage

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,56 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import ReactGA from 'react-ga';
+import { shallow, mount } from 'enzyme';
 import App from './App';
 
-jest.mock("react-ga");
+jest.mock('react-ga');
+HTMLCanvasElement.prototype.getContext = () => jest.fn();
 
-test('Account delete view renders', () => {
+const setStateSpy = jest.spyOn(App.prototype, 'setState');
+
+afterEach(() => {    
+  jest.clearAllMocks();
+});
+
+test('App snapshot rendering', () => {
   const wrapper = shallow(<App />);
   expect(wrapper).toMatchSnapshot();
+});
+
+test('should initialise userData if no userData', () => {
+  const getItemSpy = jest.spyOn(Storage.prototype, 'getItem');
+  
+  shallow(<App />);
+
+  expect(getItemSpy).toHaveBeenCalled();
+  expect(setStateSpy).toHaveBeenCalledWith({userData: {
+    appStep: 0,
+    userKey: 'C'
+  }});
+});
+
+test('should initialise analytics', () => {
+  const reactGaInitSpy = jest.spyOn(ReactGA, 'initialize');
+  const reactGaPgViewSpy = jest.spyOn(ReactGA, 'pageview');
+
+  shallow(<App />);
+  expect(reactGaInitSpy).toHaveBeenCalledWith('UA-146065324-1');
+  expect(reactGaPgViewSpy).toHaveBeenCalledWith('/home');
+
+});
+
+test('should update user key when continuing through to bar-view', () => {
+  const newKey = 'D';
+  const wrapper = mount(<App />);
+  wrapper.find('#userKeyInput').simulate('change', { target: { value: newKey } });
+  expect(setStateSpy).toHaveBeenCalledWith({userData: {
+    appStep: 0,
+    userKey: newKey
+  }});
+  wrapper.find('#continueToBarViewBtn').simulate('click');
+  expect(setStateSpy).toHaveBeenCalledWith({userData: {
+    activeBarNumber: 1,
+    appStep: 1,
+    userKey: newKey
+  }});
 });
