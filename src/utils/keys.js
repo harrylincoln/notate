adff072d4eb4eb69336d9f68c9f750b774ed5ee1
Eric Turner <eric@ericturner.dev>
2019-11-08 21:54:48 -0500

    Fix code smell caught by eslint during the build

--- src/utils/keys.js
+++ src/utils/keys.js
@@ -1,10 +1,11 @@
 exports.normalize = function normalize(key) {
-    const k = key || '';
+    let k = key || '';
 
     if (k.length > 1) {
         // Uppercase just the first letter
-        return k[0].toUpperCase() + k.slice(1);
+        k = k[0].toUpperCase() + k.slice(1);
     } else {
-        return k.toUpperCase();
+        k = k.toUpperCase();
     }
+    return k;
 };
