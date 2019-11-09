f77cb06d07d2f5a72897e89174fef937509c404f
harrylincoln <harrylincoln0612@gmail.com>
2019-11-09 12:16:29 +0000

    Jest up and running with basic snapshot testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+jest.mock("react-ga");
+
+test('Account delete view renders', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper).toMatchSnapshot();
 });
